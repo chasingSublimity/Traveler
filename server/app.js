652d5fe9195fc3b057949c96368dedbfa172844d
chasingSublimity <blakemsager@gmail.com>
2017-04-19 13:50:24 -0500

    add app.js to handle routes and middleware. add config.js to configure databases

---
+++ server/app.js
@@ -0,0 +1,25 @@
+// we've separated out our app and server. `app`
+// is responsible for coordinating routes and middleware.
+// server is responsible for serving the app defined
+// in this file.
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+
+const tripsRouter = require('./routes/trips');
+
+
+// Set up the express app
+const app = express();
+
+app.use(morgan('common'));
+app.use(bodyParser.json());
+
+app.use('/restaurants', tripsRouter);
+
+app.use('*', function(req, res) {
+	res.status(404).json({message: 'Not Found'});
+});
+
+module.exports = app;
