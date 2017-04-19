9b9a366bca48c1d66e4aeb6ed019953c10bd14ee
chasingSublimity <blakemsager@gmail.com>
2017-04-19 16:07:11 -0500

    add 'created_at' and 'updated_at' columns to both tables. Sequelize needs these to be explicitly defined on table creation to work.

---
+++ traveler-db.sql
@@ -0,0 +1,20 @@
+CREATE TABLE trips (
+  id serial PRIMARY KEY,
+  origin text NOT NULL,
+  destination text NOT NULL,
+  begin_date date NOT NULL,
+  end_date date NOT NULL,
+  created_at timestamp DEFAULT now(),
+  updated_at timestamp DEFAULT now()
+);
+
+CREATE TABLE memories (
+  id serial PRIMARY KEY,
+  trip_id int REFERENCES trips ON DELETE CASCADE,
+  img_url text,
+  location text NOT NULL,
+  date_created date NOT NULL,
+  comments text,
+  created_at timestamp DEFAULT now(),
+  updated_at timestamp DEFAULT now()
+);
