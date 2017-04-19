ff6ffc194eb4bb45ba0f2c8257dae1d7ab2f4a6d
chasingSublimity <blakemsager@gmail.com>
2017-04-19 13:11:43 -0500

    rename files to singular form, reformat document so eslint will stop yelling at me, add model index.js

--- server/models/trip.js
+++ server/models/trip.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 // we need the Sequelize library in order to
 // get the different data types for model properties
@@ -9,73 +9,76 @@ const Sequelize = require('sequelize');
 // files.
 const {sequelize} = require('../db/sequelize');
 
-const Trip = sequelize.define('Trip', {
-    origin: {
-      type: Sequelize.TEXT,
-      allowNull: false
-    },
-    destination: {
-      type: Sequelize.TEXT,
-      allowNull: false
-    },
-    beginDate: {
-      type: Sequelize.DATE,
-      field: 'begin_date',
-      allowNull: false
-    },
-    endDate: {
-      type: Sequelize.DATE,
-      field: 'end_date',
-      allowNull: false
-    }
-  }, {
+const Trip = sequelize.define('Trip', 
+	{
+		origin: {
+			type: Sequelize.TEXT,
+			allowNull: false
+		},
+		destination: {
+			type: Sequelize.TEXT,
+			allowNull: false
+		},
+		beginDate: {
+			type: Sequelize.DATE,
+			field: 'begin_date',
+			allowNull: false
+		},
+		endDate: {
+			type: Sequelize.DATE,
+			field: 'end_date',
+			allowNull: false
+		}
+	}, {
     // we explicitly tell Sequelize that this model is linked
     // to a table named 'trips' instead of having Sequelize
     // automatically determine table names, which can be error
     // prone
-    tableName: 'trips',
+		tableName: 'trips',
     // this options ensures that if sequelize creates any
     // tables on behalf of this model. Not currently necessary,
     // but leaving it in case of future changes
-    underscored: true,
-    getterMethods: {
+		underscored: true,
+		getterMethods: {
       // none right now, but may use getters in the future
-    },
-    classMethods: {
+		},
+		classMethods: {
       // relations between models are declared in `.classMethods.associate`, which gets called
       // in `/models/index.js`
-      associate: function(models) {
-        Trip.hasMany(
+			associate: function(models) {
+				Trip.hasMany(
           models.Memories,
-          {
-            as: 'memories',
-            // this is how we make memory.trip_id non-nullable
-            foreignKey: { allowNull: false },
-            // when a trip is deleted, that should cascade
-            // to its memories. note that this correlates with the
-            // relationships we've established in
-            // the shell script
-            onDelete: 'CASCADE'
-          });
-      }
-    },
-    instanceMethods: {
+					{
+						as: 'memories',
+						// this is how we make memory.trip_id non-nullable
+						foreignKey: { allowNull: false },
+						// when a trip is deleted, that should cascade
+						// to its memories. note that this correlates with the
+						// relationships we've established in
+						// the shell script
+						onDelete: 'CASCADE'
+					});
+			}
+		},
+		instanceMethods: {
       // we'll use this instance method to create a standard
       // standard representation of this resource in our app.
-      apiRepr: function() {
-        return {
-          id: this.id,
-          origin: this.origin,
-          destination: this.destination,
-        }
-      }
-    }
-  }
+			apiRepr: function() {
+				return {
+					id: this.id,
+					origin: this.origin,
+					destination: this.destination,
+					beginDate: this.beginDate,
+					endDate: this.endDate
+				};
+			}
+		}
+	}
 );
 
 // Although we export `Restaurant` here, any code that needs `Restaurant`
 // should import it from `./models/index.js` (so, for instance,
 // `const {Restaurant} = require('./models')`).
 module.exports = {
-  Trip
+	Trip
 };
