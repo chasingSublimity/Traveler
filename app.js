97be6976b56e2313c9bf20c9f3f3aa19d9a9076e
chasingSublimity <blakemsager@gmail.com>
2017-05-04 19:25:19 -0500

    edit middleware to allow cross-origin responses

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ const app = express();
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
+// allow cross origin requests
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 app.use('/users', usersRouter);
 app.use('/trips', tripsRouter);
 app.use('/memories', memoriesRouter);
