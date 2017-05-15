54d70f32ee53a0c18d79a72ae7aae6ae6fd14ed2
chasingSublimity <blakemsager@gmail.com>
2017-05-15 10:59:07 -0500

    edit trips router to accomdate authentication

--- routes/trips.js
+++ routes/trips.js
@@ -3,17 +3,25 @@ const router = express.Router();
 
 const {Trip, Memory, User} = require('../models');
 
-// get trips
-router.get('/', (req, res) => Trip.findAll(
-	{
-		include: [{
-			model: Memory,
-			as: 'memories'
-		}]
-	}).then(trips => res.json({
-		trips: trips.map(trip => trip.apiRepr())
-	}))
-);
+// get trips by grabbing the id from the user and then finding trips belonging to that id
+router.get('/', (req, res) => {
+	console.log('query: ', req.query);
+	User.find({
+		where: {userName: req.query.userName}
+	}).then(user => {Trip.findAll(
+		{
+			where: {
+				userId: user.id
+			},
+			include: [{
+				model: Memory,
+				as: 'memories'
+			}]
+		})
+	.then(trips => res.json({
+		trips: trips.map(trip => trip.apiRepr())}));
+	});
+});
 
 // can get individual trips by id
 router.get('/:id', (req, res) => Trip.findById(req.params.id, {
